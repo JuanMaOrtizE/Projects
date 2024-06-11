316ae5fe8e0703637051b7de1201e8850c4bb3e0
JuanMaOrtizE <53711761+JuanMaOrtizE@users.noreply.github.com>
2024-06-10 22:17:34 -0500

    evento scroll nav bar

--- bankistMarketing/script.js
+++ bankistMarketing/script.js
@@ -1,13 +1,15 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -32,9 +34,6 @@ document.addEventListener('keydown', function (e) {
 
 //SCROLL
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener(`click`, function (e) {
   // const sec1Coord = section1.getBoundingClientRect();
   // console.log(sec1Coord);
@@ -43,4 +42,22 @@ btnScrollTo.addEventListener(`click`, function (e) {
   // window.scrollTo(sec1Coord.left, sec1Coord.top + window.pageYOffset);
 
   section1.scrollIntoView({ behavior: `smooth` });
+});
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener(`click`, function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute(`href`);
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener(`click`, function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains(`nav__link`)) {
+    const id = e.target.getAttribute(`href`);
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+  }
 });
